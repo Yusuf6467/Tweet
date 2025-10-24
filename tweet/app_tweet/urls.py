60d4867c04b183b34fb6eb1c1772999b95ef879b
Yusuf6467 <yusufkhan10723@gmail.com>
2025-10-24 16:28:19 +0530

    added first inside under navbar

--- tweet/app_tweet/urls.py
+++ tweet/app_tweet/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('/', views.tweet, name='tweet')
+    path('', views.tweet, name='tweet')
 ]
--- tweet/tweet/urls.py
+++ tweet/tweet/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet', include('app_tweet.urls')),
+    path('tweet/', include('app_tweet.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
